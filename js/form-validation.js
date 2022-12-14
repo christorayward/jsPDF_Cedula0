b0a5cf4d65336dd80ad2422493878103569ab757
Christopher <62074270+christorayward@users.noreply.github.com>
2022-12-14 14:06:51 -0600

    Add files via upload

--- js/form-validation.js
+++ js/form-validation.js
@@ -1,10 +1,9 @@
 // Example starter JavaScript for disabling form submissions if there are invalid fields
-(function validation () {
+(function validation() {
   'use strict'
 
   // Fetch all the forms we want to apply custom Bootstrap validation styles to
   var forms = document.querySelectorAll('.needs-validation')
-
   // Loop over them and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
@@ -12,9 +11,12 @@
         if (!form.checkValidity()) {
           event.preventDefault()
           event.stopPropagation()
+          document.getElementById('btncedula0').setAttribute("disabled", ""); //Se agregó el else con esta sentencia para que si el usuario cambia un dato y no es válido, vuelve a desactivar la generación de cédula
+        }else{
+          document.getElementById('btncedula0').removeAttribute('disabled'); //Quita el atributo disabled del botón para generar la cédula 0
         }
-
         form.classList.add('was-validated')
+        
       }, false)
     })
 })()
